40c2de045e0d17304cecd9cef7461db05590a980
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-09-18 15:06:17 -0400

    config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ var config = {}
 
 config.duration = 4.0;
 
+// Example: {image: 'ding.png', file: 'bss_eggo_dingthing_ding_', count:17, min: 1.5, max: 3.0},
 config.voices = [
-    {image: 'toaster.png', file: 'bss_eggo_dingthing_toaster_', count:11, max: 1.5},
+    {image: 'logo.png', file: 'bss_tide_spincycle_vocals_', count:20, min: 0, max: 1.5},
     {image: 'ding.png', file: 'bss_eggo_dingthing_ding_', count:17, min: 1.5, max: 3.0},  
     {image: 'waffle.png', file: 'bss_eggo_dingthing_waffle_', count:6, min: 3.0}, 
     {image: 'leggo.png', file: 'bss_eggo_dingthing_leggo_', count:11}
