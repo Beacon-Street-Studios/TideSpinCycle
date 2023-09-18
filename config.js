e6c97213f94a746fd9d693c8da83db702eaf6f84
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-09-18 15:24:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,10 @@ config.duration = 4.0;
 // Example: {image: 'ding.png', file: 'bss_eggo_dingthing_ding_', count:17, min: 1.5, max: 3.0},
 config.voices = [
     {image: 'logo.png', file: 'bss_tide_spincycle_vocals_', count:20, min: 0, max: 1.5},
-    {image: 'ding.png', file: 'bss_eggo_dingthing_ding_', count:17, min: 1.5, max: 3.0},  
-    {image: 'waffle.png', file: 'bss_eggo_dingthing_waffle_', count:6, min: 3.0}, 
-    {image: 'leggo.png', file: 'bss_eggo_dingthing_leggo_', count:11}
+    {image: 'ding.png', file: 'bss_tide_spincycle_swell', count:14, min: 1.5, max: 3.0},  
+    {image: 'waffle.png', file: 'bss_tide_spincycle_chime_', count:9, min: 3.0}, 
+    {image: 'leggo.png', file: 'bss_tide_spincycle_melody_', count:21},
+    {image: 'toaster.png', file: 'bss_tide_spincycle_sfx_', count:12}
 ];
 
 config.voiceCount = config.voices.length;
