e4224cc63219aa8e2e1e1c8b7c9d3486dafa2bc7
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-09-18 23:14:50 -0400

    Update config.js
    Updated random weights

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ config.duration = 4.0;
 
 // Example: {image: 'ding.png', file: 'bss_eggo_dingthing_ding_', count:17, min: 1.5, max: 3.0},
 config.voices = [
-    {image: 'vocals.png', file: 'bss_tide_spincycle_vocals_', count:20, min: 0, max: 1.5},
-    {image: 'swell.png', file: 'bss_tide_spincycle_swell_', count:14, min: 1.5, max: 3.0},  
-    {image: 'chime.png', file: 'bss_tide_spincycle_chime_', count:9, min: 3.0}, 
-    {image: 'melody.png', file: 'bss_tide_spincycle_melody_', count:21},
-    {image: 'sfx.png', file: 'bss_tide_spincycle_sfx_', count:12}
+    {image: 'vocals.png', file: 'bss_tide_spincycle_vocals_', count:20, min: 1, max: 3.5},
+    {image: 'swell.png', file: 'bss_tide_spincycle_swell_', count:14, min: 0, max: 1.5},  
+    {image: 'chime.png', file: 'bss_tide_spincycle_chime_', count:9, min: 1.5, max: 3.5}, 
+    {image: 'melody.png', file: 'bss_tide_spincycle_melody_', count:21, min: 1.5, max: 3.5},
+    {image: 'sfx.png', file: 'bss_tide_spincycle_sfx_', count:12, min: 0, max: 1.5}
 ];
 
 config.voiceCount = config.voices.length;
